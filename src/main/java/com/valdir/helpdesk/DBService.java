ba9f1f7af66c77bb9e427b72ef4f540040929b93
daniel <danielsilvacr123@gmail.com>
2023-03-01 15:13:47 -0300

    Perfil de teste criado

---
+++ src/main/java/com/valdir/helpdesk/DBService.java
@@ -0,0 +1,42 @@
+package com.valdir.helpdesk;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valdir.helpdesk.domain.Chamado;
+import com.valdir.helpdesk.domain.Cliente;
+import com.valdir.helpdesk.domain.Tecnico;
+import com.valdir.helpdesk.domain.enums.Perfil;
+import com.valdir.helpdesk.domain.enums.Prioridade;
+import com.valdir.helpdesk.domain.enums.Status;
+import com.valdir.helpdesk.reposirories.ChamadoRepository;
+import com.valdir.helpdesk.reposirories.ClienteRepository;
+import com.valdir.helpdesk.reposirories.TecnicoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public void instanciaDB() {
+		Tecnico tec1 = new Tecnico(null, "Valdir cezar", "123.674.456-00", "Valdir@gmail.com", "123");
+		tec1.addPerfis(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torval", "233.456.544-56", "Linus@gmail.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+	
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+
+}
