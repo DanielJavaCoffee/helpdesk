ce5f5a41077634f119b9451178d068dc18f7bfd4
daniel <danielsilvacr123@gmail.com>
2023-03-03 18:01:36 -0300

    endpoint findall para tecnicos

--- src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.valdir.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,5 +25,12 @@ public class TecnicoResource {
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico tecnico = tecnicoService.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(tecnico));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		List<Tecnico> tecnicos = tecnicoService.findAll();
+		List<TecnicoDTO> tecnicoDTOs = tecnicos.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(tecnicoDTOs);
 	}
 }
